f93e3c262c8f0f68c7b3883486b386e9513fca31
Tamoyaaaaa <7amoyka@gmail.com>
2024-07-03 18:37:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace _7towproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            {
+                Console.WriteLine("Введите число:");
+                number = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            int i = 0;
+
+            while (i <= number)
+            {
+                sum = sum + i;
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int sum = 0;
+
+            int number = 10;
+            int i = 0;
+            while (i <= number)
+            {
+                sum = sum + i;
+                i++;
+            }
+            Console.WriteLine(sum); */
